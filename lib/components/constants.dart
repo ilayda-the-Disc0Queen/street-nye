f67e09689f663b4a0994b867d06de6a394d22baa
Ilayda <ilayda.bakare@gmail.com>
2021-12-02 15:43:52 +0000

    Background image full width with text over

---
+++ lib/components/constants.dart
@@ -0,0 +1,16 @@
+import 'dart:ui';
+
+class Constants {
+  static const double appBarHeight = 110.0;
+
+  // APP COLOURS
+  static const Color matrixGreenColor = Color(0xFF07AB03);
+  static const Color lightPrimaryColor = Color(0xFF104e8e);
+  static const Color darkPrimaryColor = Color(0xFF052B5F);
+  static const Color mainSecondaryColor = Color(0xFFFFAB00);
+  static const Color lightSecondaryColor = Color(0xFFFBD040);
+  static const Color darkSecondaryColor = Color(0xFFC67C00);
+
+  static const Color offWhiteColor = Color(0xFFecf4fe);
+  static const Color darkBlueTextColor = Color(0xFF061E37);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:street_nye/components/banner_image.dart';
-import 'package:video_player/video_player.dart';
+
+import 'components/constants.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,47 +15,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Street NYE',
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        textTheme: GoogleFonts.pressStart2pTextTheme(
+          Theme.of(context).textTheme,
+        ),
       ),
-      home: const HomePage(),
+      title: 'Street NYE',
+      home: HomePage(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
-//
-// class _MyHomePageState extends State<MyHomePage> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             BannerImage(
-//                 imagePath: 'assets/images/matrix_gif.gif',
-//                 altText: 'matrix characters',
-//                 overImageText: 'Do you dare to enter the Matrix?'),
-//             // FittedBox(
-//             //   child: Image.asset('assets/images/matrix.gif'),
-//             //   fit: BoxFit.fill,
-//             // )
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-//FittedBox(
-//           child: Image.asset('assets/images/matrix_gif.gif'),
-//           fit: BoxFit.fill,
-//         )
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -69,12 +42,12 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   void initState() {
     super.initState();
     _controller = AnimationController(
-      duration: Duration(seconds: 5),
+      duration: const Duration(seconds: 5),
       vsync: this,
     )..repeat(reverse: true);
 
-    _color =
-        ColorTween(begin: Colors.green, end: Colors.white).animate(_controller);
+    _color = ColorTween(begin: Constants.matrixGreenColor, end: Colors.white)
+        .animate(_controller);
   }
 
   @override
@@ -86,59 +59,55 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
-        title: Text('Welcome to Street NYE'),
+        title: const Text(
+          'Welcome to Street NYE',
+        ),
+        backgroundColor: Constants.matrixGreenColor,
       ),
-      body: Container(
-        alignment: Alignment.center,
-        color: Colors.black,
-        child: AnimatedBuilder(
-          animation: _color,
-          builder: (BuildContext _, Widget? __) {
-            // return Container(
-            //   width: 300,
-            //   height: 300,
-            //   decoration:
-            //       BoxDecoration(color: _color.value, shape: BoxShape.circle),
-            // );
-            return Center(
-              child: Stack(
-                alignment: Alignment.center,
-                children: <Widget>[
-                  Container(
-                    color: Colors.black,
-                    child: Opacity(
-                        opacity: 1,
-                        child: Image.asset(
-                          'assets/images/matrix_gif.gif',
-                          semanticLabel: 'Binary Matrix rain',
-                          fit: BoxFit.cover,
-                        )),
-                  ),
-                  Center(
-                    // child: Text(
-                    //   'Do you want to yes',
-                    //   style: TextStyle(
-                    //     color: _color,
-                    //   ),
-                    // ),
-                    child: AnimatedDefaultTextStyle(
+      body: Column(
+        children: [
+          SizedBox(
+            height: 800,
+            child: AnimatedBuilder(
+              animation: _color,
+              builder: (BuildContext _, Widget? __) {
+                return Stack(
+                  alignment: Alignment.center,
+                  children: <Widget>[
+                    Container(
+                      color: Colors.black,
+                      child: Opacity(
+                          opacity: 1,
+                          child: Image.asset(
+                            'assets/images/matrix_gif.gif',
+                            semanticLabel: 'Binary Matrix rain',
+                            fit: BoxFit.cover,
+                            height: double.infinity,
+                            width: double.infinity,
+                          )),
+                    ),
+                    AnimatedDefaultTextStyle(
                       duration: const Duration(milliseconds: 200),
-                      // style: isSelected
-                      //     ? const TextStyle(color: Colors.green)
-                      //     : const TextStyle(color: Colors.white),
-                      style: const TextStyle(fontSize: 40.0),
-                      child: Text(
-                        'Do you dare to enter the Matrix?',
-                        style: TextStyle(color: _color.value),
+                      style:
+                          const TextStyle(fontSize: 70.0, letterSpacing: 8.0),
+                      child: Container(
+                        margin: const EdgeInsets.all(20.0),
+                        child: Text(
+                          'Do you dare to enter the Matrix?',
+                          style: GoogleFonts.pressStart2p(
+                              color: _color.value, fontSize: 20.0),
+                          textAlign: TextAlign.center,
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            );
-          },
-        ),
+                  ],
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
